fbca402603696894efaf503245d0c787c8f50648
syarasyojyu <watast.u.1764@gmail.com>
2024-06-03 10:07:10 +0900

    AIが採点

--- script/inaba/recognize_face.py
+++ script/inaba/recognize_face.py
@@ -12,7 +12,7 @@ def send_request_to_gpt4o(messages:list)->str|None:
         response = completion(
             model="gpt-4o",
             messages=messages,
-            api_key=os.environ['OPENAI_API_KEY'] 
+            api_key=os.environ['OPENAI_API_KEY'],
         )
         print(response)
         return response['choices'][0]['message']['content']
@@ -23,7 +23,7 @@ def send_request_to_gpt4o(messages:list)->str|None:
 def make_title()->str:
     """タイトルを作成する関数"""
     title_content="""あなたは、とても楽しく、友達たちとエンジョイできるようなパーティーの主催者です。そこで、
-あなたはどれだけお題に沿った顔を披露できるかというゲームを考えました。このゲームにピッタリで、採点できそうなお題を1つだけ考えてください。
+あなたはどれだけお題に沿った顔を披露できるかというゲームを考えました。このゲームにピッタリで、採点できそうなお題を1つだけ考えてくださいできるだけパーティーの盛り上がりにつながるようなお題を考えてください。
 なお、お題以外の情報を入力することを禁止します。
 例：
 移動教室を間違えた顔
@@ -52,17 +52,20 @@ def Judje(title:str,file_path:str)->str:
 
     # data URIスキームに従ってフォーマットする
     image_url = f"data:image/png;base64,{encoded_string}"
-    content_template="""お題[{{title}}]
-にどれだけ沿っている顔かを、100点満点でシビア目に採点してください。
-その後、その顔に関してピッタリのお題を１つ教えてください。
-なお、画像は{{image_url}}です。"""
+    content_template="""
+お題[{{title}}]にどれだけ沿っている顔かを、100点満点でシビア目に採点し、その点数をpointに書いてください。
+その後、採点理由をdescriptionに簡潔に記述してください。
+なお、画像は{{image_url}}です。
+例:
+{'point': 60, 'description': '恋人にとられた悲しさをしっかり表現しています。しかし、全体的に驚きの感情の強さが不足しており、眉や口の動きがやや弱いように感じられます。もう少し強い感情表現が必要です。'},
+{'point': 30, 'description': '目の前に好きなアイドルが現れた際の驚きや喜びがあまり伝わってきません。見た目にもう少し輝きや表情の明確な変化があると点数が上がるでしょう'}"""
     content=Template(content_template).render(title=title,image_url=image_url)
     print(len(image_url))
     # メッセージリストを作成
     messages = [
         {
             "role": "system",
-            "content": "点数とお題のみを出力してください。点数とお題以外を出力することを禁止します。"
+            "content": "あなたは返答をすべてJSON形式で出力します。{'point':,'description':}の形式で出力してください。"
             },
         {
             "role": "user",
@@ -77,7 +80,7 @@ def Judje(title:str,file_path:str)->str:
 
 if __name__ == '__main__':
     with open ('script/inaba/face_paths.txt','rb') as f:
-        file_paths=f.read().splitlines()
+        file_paths=f.read().splitlines()[:1]
     title=make_title()
     for file_path in file_paths:
         Judje(title,file_path)
